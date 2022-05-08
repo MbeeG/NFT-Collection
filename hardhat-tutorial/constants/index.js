00b9d9d17819314c5860392290653af5ed7c57a8
abbayo <abbabgm@gmail.com>
2022-05-08 09:15:05 +0100

    new file:   README.md 	new file:   hardhat-tutorial/README.md 	new file:   hardhat-tutorial/constants/index.js 	new file:   hardhat-tutorial/contracts/CryptoDevs.sol 	new file:   hardhat-tutorial/contracts/IWhitelist.sol 	new file:   hardhat-tutorial/hardhat.config.js 	new file:   hardhat-tutorial/scripts/deploy.js

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x31cbDDB620eCd1b13D59c610efC8eBEDfc599224";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
